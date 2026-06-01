894673e32c53fddf291db4ea1d84b261ccea4e3d
sigmatrickspk-tech <sigmatrickspk@gmail.com>
2026-06-01 20:35:34 +0500

    Change BOT_TOKEN and ADMIN_IDS in config.py
    Updated BOT_TOKEN and ADMIN_IDS with new values.

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 from dotenv import load_dotenv
 load_dotenv()
-BOT_TOKEN = os.getenv("BOT_TOKEN", "")
-ADMIN_IDS = [int(x) for x in os.getenv("ADMIN_IDS", "0").split(",") if x.strip().isdigit()]
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8651241172:AAHPoKF8-Nxc9ibav7Q9BITnnmFCDRQNWdc")
+ADMIN_IDS = [int(x) for x in os.getenv("ADMIN_IDS", "8278238550").split(",") if x.strip().isdigit()]
 DB_FILE = "database.sqlite3"
